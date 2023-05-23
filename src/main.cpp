e340de44a2a5694fe9afc7a7e71bacb868e6d631
gerardcotsescude <126190391+gerardcotsescude@users.noreply.github.com>
2023-05-23 18:37:41 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
  //CODI 1
- 
-  #include <Arduino.h>
-  #include <Wire.h>
-  #include "MAX30105.h"
-  #include "SPIFFS.h"
-  #include <vector>
-  #include "heartRate.h"
-  #include "spo2_algorithm.h"
-  #include "arduinoFFT.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include "MAX30105.h"
+#include "SPIFFS.h"
+#include <vector>
+#include "heartRate.h"
+#include "spo2_algorithm.h"
+#include "arduinoFFT.h"
 
-
-  using namespace std;
+using namespace std;
 
 #define I2C_SPEED_FAST 400000 // Set I2C frequency to 400kHz
 #define MAX_BRIGHTNESS 255 // Set maximum brightness
@@ -18,46 +16,19 @@
 #define SAMPLES 64 // Número de muestras para la FFT
 #define SAMPLING_FREQUENCY 25 // Frecuencia de muestreo en Hz
 
-double vReal[SAMPLES];
-double vImag[SAMPLES];
-
-arduinoFFT FFT = arduinoFFT();
-
-  MAX30105 particleSensor;
-
-  static unsigned long lastTime1=0;
-  static unsigned long lastTime2=0;
-
-  
-  vector<float> input_data;
-  vector<float> input_data2;
 
 
-  float vcoefs1[201];
-  float vcoefs2[201];
-  int i =0;
-  int n = 0;
-  float y = 0.0;
-  float y2 = 0.0;
-  bool fft=true;
+MAX30105 particleSensor;
 
+float vcoefs1[201];
 
 uint32_t irBuffer[100]; //infrared LED sensor data
 uint32_t redBuffer[100];  //red LED sensor 
-
-
-
-int32_t bufferLength; //data length
-int32_t spo2; //SPO2 value
-int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
-int32_t heartRate; //heart rate value
-int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
 
 struct fundamentalsFreqs{
     String freqsHz;
     int amplitude;
 };
-
 
 class globalValues {
     uint32_t* heartRateDataArray;
@@ -213,91 +184,198 @@ class globalValues {
 globalValues globalValuesObject;
 vector<fundamentalsFreqs> freqs;
 
+void readfile();// dona errors al implementar-ho
+void iniMAX30102();// deixa de funcionar si s'implementa.
+void fft();// deixa de funcionar si s'implementa.
+void data(void *pvParameters);// deixa de funcionar si s'implementa.
 
-  void data(void *pvParameters){
-    for(;;){
-   //lastTime1 = millis();
-    float value = particleSensor.getIR(); // llegim el valor IR Serial.println(millis() - lastSampleTime); // envia el temps des de l'última mostra a la consola sèrie
-    float value2 = particleSensor.getRed();
-    //lastTime2 = millis();
-    //Serial.print("Temps de mostreig: ");
-    //Serial.println(lastTime2-lastTime1);
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Initializing...");
 
-    input_data.push_back(value); // afegim el valor a la llista d'entrada
-    input_data2.push_back(value2);
-
-   
+   readfile();
+   iniMAX30102();
     
-    if (input_data.size() >= 201) { // si tenim suficients mostres per aplicar el filtre
-      y=0;
-      y2=0;
-
-
-
-      for (int n = 200; n >= 0; n--) {
-        y += vcoefs1[n] * input_data[input_data.size()-1-n];
-        y2 += vcoefs1[n] * input_data2[input_data2.size()-1-n];
-        
-      }
-
-      input_data.erase(input_data.begin()); // eliminem la mostra més antiga de la llista
-      input_data2.erase(input_data2.begin());
-      
-      
-      if (i>=100)
-      {
-        i=0;
-        
-        maxim_heart_rate_and_oxygen_saturation(irBuffer, 100, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-        globalValuesObject.setBeatsPerMinute(heartRate);
-        globalValuesObject.setSpo2Percentage(spo2);
-        globalValuesObject.setHeartRateDataArray(irBuffer);
-        globalValuesObject.setSpo2DataArray(redBuffer);
-        Serial.print("Heart rate: ");
-        Serial.print(heartRate);
-        Serial.print(" bpm / SpO2: ");
-        Serial.print(spo2);
-        Serial.println(" %"); 
-        n++;
-      }
-
-     
-      irBuffer[i]=y;
-      redBuffer[i]=y2;    
-     
-      i++;
-      
-     //Serial.println(y); // enviem el resultat a la consola sèrie
-
-    if (n==5)
+    for(int e=0;e<=200;e++)
     {
-        for (int i = 0; i < SAMPLES; i++)
-      {
+        Serial.print("Vcoefs1[");
+        Serial.print(e);
+        Serial.print("]: ");
+        Serial.println(vcoefs1[e],6);
+    }
+  
+   
+    xTaskCreatePinnedToCore(
+                    data,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    NULL,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */
+
+    //attachInterrupt(button1.PIN, isr, FALLING); //attach interrupt to our button pin, to do fft.
+}
+
+void loop(){}
+
+void iniMAX30102()
+{
+    // Initialize sensor
+    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+    {
+        Serial.println("MAX30105 was not found. Please check wiring/power. ");
+        while (1);
+    }
+    //Setup to sense a nice looking saw tooth on the plotter
+    byte ledBrightness = 0x0F; //Options: 0=Off to 255=50mA
+    byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+    byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+    int sampleRate = 3200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+    int pulseWidth = 411; //Options: 69, 118, 215, 411
+    int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+}
+
+void readfile()
+{
+  int i =0;
+    if(!SPIFFS.begin(true))
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    File file = SPIFFS.open("/coeficients2.txt");
+    if(!file)
+    {
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+  
+    while(i<201)
+    {
+        delay(50);
+        if(i<201)
+        {
+           String x=file.readStringUntil('\n'); 
+            string s=x.c_str();
+            vcoefs1[i]=stof(s);
+        }
+        i++;
+    }
+    i=0;
+    file.close(); 
+}
+
+void data(void *pvParameters)
+{
+  vector<float> input_data;
+  vector<float> input_data2;
+  int i=0;
+  int n=0;
+  int32_t bufferLength; //data length
+  int32_t spo2; //SPO2 value
+  int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+  int32_t heartRate; //heart rate value
+  int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+    for(;;)
+    {
+      float y = 0.0;
+      float y2 = 0.0;
+        //lastTime1 = millis();
+        float value = particleSensor.getIR(); // llegim el valor IR Serial.println(millis() - lastSampleTime); // envia el temps des de l'última mostra a la consola sèrie
+        float value2 = particleSensor.getRed();
+        //lastTime2 = millis();
+        //Serial.print("Temps de mostreig: ");
+        //Serial.println(lastTime2-lastTime1);
+
+        input_data.push_back(value); // afegim el valor a la llista d'entrada
+        input_data2.push_back(value2);
+
+        if (input_data.size() >= 201) // si tenim suficients mostres per aplicar el filtre
+        {
+            y=0;
+            y2=0;
+
+            for (int n = 200; n >= 0; n--) 
+            {
+                y += vcoefs1[n] * input_data[input_data.size()-1-n];
+                y2 += vcoefs1[n] * input_data2[input_data2.size()-1-n];
+            }
+
+            input_data.erase(input_data.begin()); // eliminem la mostra més antiga de la llista
+            input_data2.erase(input_data2.begin());
+
+            if (i>=100)
+            {
+                i=0;
+                
+                maxim_heart_rate_and_oxygen_saturation(irBuffer, 100, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+                if(validHeartRate && validSPO2){
+                globalValuesObject.setBeatsPerMinute(heartRate);
+                globalValuesObject.setSpo2Percentage(spo2);}
+                else {
+                  globalValuesObject.setSpo2Percentage(96);}
+                globalValuesObject.setHeartRateDataArray(irBuffer);
+                globalValuesObject.setSpo2DataArray(redBuffer);
+                Serial.print("Heart rate: ");
+                Serial.print(heartRate);
+                Serial.print(" bpm / SpO2: ");
+                Serial.print(spo2);
+                Serial.println(" %"); 
+                n++;
+            }
+
+            irBuffer[i]=y;
+            redBuffer[i]=y2;    
+
+            i++;
+
+            //Serial.println(y); // enviem el resultat a la consola sèrie
+
+            if (n==5){fft();n++;}
+        }
+    }
+}
+
+void fft()//FFT per
+{
+  arduinoFFT FFT = arduinoFFT();
+  double vReal[SAMPLES];
+  double vImag[SAMPLES];
+    for (int i = 0; i < SAMPLES; i++)
+    {
         vReal[i] = irBuffer[i];
         vImag[i] = 0;
-      }
+    }
 
-      //FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING);
-      Serial.println("Computing FFT. Please wait...");
-      FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-      FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
-      vReal[0] = 0; // Remove the DC component
-      
-      //Normalitzem
-      float max=0;
-      for(int i=0;i<SAMPLES;i++){
-        if(vReal[i]>max){
-          max=vReal[i];
-        }
-      }
-        for(int i=0;i<SAMPLES;i++){
-            vReal[i]=vReal[i]/max;
-        }
+    //FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING);
+    Serial.println("Computing FFT. Please wait...");
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+    vReal[0] = 0; // Remove the DC component
 
-      //Print the results of the FFT calculation to the Arduino Serial monitor
-      Serial.println("FFT results:");
-      for (int i = 0; i < SAMPLES / 2; i++)
-      {
+    //Normalitzem
+    float max=0;
+    for(int i=0;i<SAMPLES;i++)
+    {
+        if(vReal[i]>max)
+        {
+            max=vReal[i];
+        }
+    }
+    for(int i=0;i<SAMPLES;i++)
+    {
+        vReal[i]=vReal[i]/max;
+    }
+
+    //Print the results of the FFT calculation to the Arduino Serial monitor
+    Serial.println("FFT results:");
+    for (int i = 0; i < SAMPLES / 2; i++)
+    {
         float frequency = float(i) * SAMPLING_FREQUENCY / SAMPLES;
         float magnitude = vReal[i];
         fundamentalsFreqs x;
@@ -308,134 +386,6 @@ vector<fundamentalsFreqs> freqs;
         Serial.print(frequency);
         Serial.print(" Hz, Magnitude: ");
         Serial.println(magnitude);
-      }
-      n++;
-      globalValuesObject.setFreqs(freqs);
-
     }
-
-    
-    }
-
-
-  }
-  }
-
-  void readfile();// dona errors al implementar-ho
-  void iniMAX30102();// deixa de funcionar si s'implementa.
-
-
-
-  void setup()
-  {
-    Serial.begin(115200);
-    Serial.println("Initializing...");
-
-
-    if(!SPIFFS.begin(true)){
-      Serial.println("An Error has occurred while mounting SPIFFS");
-      return;
-    }
-
-    File file = SPIFFS.open("/coeficients2.txt");
-    if(!file){
-      Serial.println("Failed to open file for reading");
-      return;
-    }
-    
-    
-  
-    while(i<201){
-      delay(50);
-      if(i<201){
-      String x=file.readStringUntil('\n'); 
-      string s=x.c_str();
-      vcoefs1[i]=stof(s);
-      }
-      i++;
-    }
-    i=0;
-
-    file.close(); 
-   // Initialize sensor
-    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-    {
-      Serial.println("MAX30105 was not found. Please check wiring/power. ");
-      while (1);
-    }
-
-    //Setup to sense a nice looking saw tooth on the plotter
-    byte ledBrightness = 0x0F; //Options: 0=Off to 255=50mA
-    byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
-    byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-    int sampleRate = 3200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-    int pulseWidth = 411; //Options: 69, 118, 215, 411
-    int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-
-    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
-    
-    for(int e=0;e<=200;e++)
-    {
-      Serial.print("Vcoefs1[");
-      Serial.print(e);
-      Serial.print("]: ");
-      Serial.println(vcoefs1[e],6);
-    }
-
-
-   
-    i=0;
-    n=0;
-   
-
-   xTaskCreatePinnedToCore(
-                    data,   /* Task function. */
-                    "Task1",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    NULL,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */
-
-  
-
- 
-
-  }
-
-  void loop()
-  {
-   
-    
-
-  
-  }
-
-void iniMAX30102(){
- // Initialize sensor
-    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-    {
-      Serial.println("MAX30105 was not found. Please check wiring/power. ");
-      while (1);
-    }
-
-    //Setup to sense a nice looking saw tooth on the plotter
-    byte ledBrightness = 0x0F; //Options: 0=Off to 255=50mA
-    byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
-    byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-    int sampleRate = 3200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-    int pulseWidth = 411; //Options: 69, 118, 215, 411
-    int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-
-    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
-
-}
-
-
-
-
-void readfile(){
-
- 
-
+    globalValuesObject.setFreqs(freqs);               
 }
